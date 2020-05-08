a158d337d459934435a7cb763fb0138344bda8c1
hawkchen <hawkchen@potix.com>
2020-05-08 18:00:08 +0800

    change implementation: init with a ListModel

--- getZkUp/src/main/java/tutorial/SearchController.java
+++ getZkUp/src/main/java/tutorial/SearchController.java
@@ -11,43 +11,47 @@ import java.util.*;
 
 public class SearchController extends SelectorComposer<Component> {
 
-	private static final long serialVersionUID = 1L;
-	
-	@Wire
-	private Textbox keywordBox;
-	@Wire
-	private Listbox carListbox;
-	@Wire
-	private Label modelLabel;
-	@Wire
-	private Label makeLabel;
-	@Wire
-	private Label priceLabel;
-	@Wire
-	private Label descriptionLabel;
-	@Wire
-	private Image previewImage;
-	
-	
-	private CarService carService = new CarServiceImpl();
-	
-	@Listen("onClick = #searchButton")
-	public void search(){
-		String keyword = keywordBox.getValue();
-		List<Car> result = carService.search(keyword);
-		carListbox.setModel(new ListModelList<Car>(result));
-	}
-	
-	@Listen("onSelect = #carListbox")
-	public void showDetail(){
-		Set<Car> selection = ((Selectable<Car>)carListbox.getModel()).getSelection();
-		if (selection!=null && !selection.isEmpty()){
-			Car selected = selection.iterator().next();
-			previewImage.setSrc(selected.getPreview());
-			modelLabel.setValue(selected.getModel());
-			makeLabel.setValue(selected.getMake());
-			priceLabel.setValue(selected.getPrice().toString());
-			descriptionLabel.setValue(selected.getDescription());
-		}
-	}
+    private static final long serialVersionUID = 1L;
+
+    @Wire
+    private Textbox keywordBox;
+    @Wire
+    private Listbox carListbox;
+    @Wire
+    private Label modelLabel;
+    @Wire
+    private Label makeLabel;
+    @Wire
+    private Label priceLabel;
+    @Wire
+    private Label descriptionLabel;
+    @Wire
+    private Image previewImage;
+
+    private ListModelList<Car> dataModel = new ListModelList<>();
+    private CarService carService = new CarServiceImpl();
+
+    @Override
+    public void doAfterCompose(Component comp) throws Exception {
+        super.doAfterCompose(comp);
+        carListbox.setModel(dataModel);
+    }
+
+    @Listen("onClick = #searchButton")
+    public void search() {
+        String keyword = keywordBox.getValue();
+        dataModel.clear();
+        dataModel.addAll(carService.search(keyword));
+    }
+
+    @Listen("onSelect = #carListbox")
+    public void showDetail() {
+        Set<Car> selection = dataModel.getSelection();
+        Car selected = selection.iterator().next();
+        previewImage.setSrc(selected.getPreview());
+        modelLabel.setValue(selected.getModel());
+        makeLabel.setValue(selected.getMake());
+        priceLabel.setValue(selected.getPrice().toString());
+        descriptionLabel.setValue(selected.getDescription());
+    }
 }
